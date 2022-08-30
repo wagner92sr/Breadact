58d34ff31e09de7c73c05c31ee8b6198f17c82c9
Wagner <wagner.rodrigues@dbccompany.com.br>
2022-08-30 11:33:10 -0300

    config inicial

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Redirect to="/production" />
+          </Route>
+
+          <Route path="/production">Produção</Route>
+
+          <Route path="/stock">Estoque</Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
